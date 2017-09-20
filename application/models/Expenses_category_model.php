6f5fc36b2f66c02ec90009410a46de8d8bcdee9f
GitHub <leanh280693@gmail.com>
2017-09-20 14:48:07 +0700

    8th

---
+++ application/models/Expenses_category_model.php
@@ -0,0 +1,6 @@
+<?php
+Class Expenses_category_model extends MY_Model
+{
+    var $table = 'sna_expenses_category';
+    
+}
---
+++ application/models/Expenses_type_model.php
@@ -0,0 +1,6 @@
+<?php
+Class Expenses_type_model extends MY_Model
+{
+    var $table = 'sna_expenses_type';
+    
+}
